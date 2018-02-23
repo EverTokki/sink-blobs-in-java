fd739fc4853f419fa058a41165512d77c26e0a56
EverTokki <chanbin.lee123@gmail.com>
2018-02-22 18:35:27 -0800

    some things

--- src/main/model/Bubble.java
+++ src/main/model/Bubble.java
@@ -1,11 +1,38 @@
 package model;
 
+import java.awt.*;
+
 public class Bubble {
 /*    private List<Bubble> Bubble;
     // ListofBoolean where 1 means that it is a Bubble,
     // 0 means it is an empty Bubble*/
 
-    public static void move(){
+// Represents a bubble.
+
+    public static final int DIAMETER = 15;
+    public static final int DY = 15;
+    public static final int X_POS = SBubbles.HEIGHT/2;
+    public static final Color COLOR = new Color(250, 128, 20);
+
+    private int y;
+
+    // EFFECT: Places bubble at position(X_POS, y)
+    public Bubble(int y){
+        this.y = y;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    // EFFECT: Moves bubble
+    public void move(){
+    y = y - DY;
+    handleBoundary();
+    }
+
+    //EFFECT: Makes sure the bubbles dont go below
+    private void handleBoundary() {
 
     }
 
